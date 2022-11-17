e5df3025d8486c188ef0211cf8adc38d5f800451
Artem Khvostyk <5hv5art@gmail.com>
2022-11-18 00:36:54 +0100

    feat: merge layouts

--- app/[locale]/layout.tsx
+++ app/[locale]/layout.tsx
@@ -3,6 +3,8 @@ import { FC, ReactNode } from 'react';
 import { Footer, Header } from '@project-management-app/widgets';
 import { AppLocale } from '@project-management-app/types';
 
+import '../../styles/global-styles.scss';
+
 type Props = {
   children: ReactNode;
   params: {
@@ -10,16 +12,18 @@ type Props = {
   };
 };
 
-const Layout: FC<Props> = ({ children, params }) => {
+const RootLayout: FC<Props> = ({ children, params }) => {
   const { locale } = params;
 
   return (
-    <>
-      <Header isAuthorized={true} />
-      {children}
-      <Footer locale={locale} />
-    </>
+    <html lang={locale}>
+      <body>
+        <Header isAuthorized={true} />
+        {children}
+        <Footer locale={locale} />
+      </body>
+    </html>
   );
 };
 
-export default Layout;
+export default RootLayout;
